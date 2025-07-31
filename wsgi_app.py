5573c48bfbb251f36f61df74a68525852f54ea19
SongBahtHaSib <banthana1234567@gmail.com>
2025-07-31 08:58:42 +0700

    Delete wsgi_app.py

--- wsgi_app.py
+++
@@ -1,5 +0,0 @@
-from app import app as application
-
-# This file is used by Vercel to find your Flask application.
-# It imports the 'app' instance from your 'app.py' file and
-# renames it to 'application' as required by Vercel's WSGI handler.
